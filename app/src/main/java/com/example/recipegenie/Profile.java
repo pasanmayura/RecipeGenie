bcac5fbc5ed54f58946aa238ec6f60794db63ba7
pasanmayura <karunat-im21020@stu.kln.ac.lk>
2024-09-17 02:01:21 +0530

    added ClickListener functions

--- app/src/main/java/com/example/recipegenie/Profile.java
+++ app/src/main/java/com/example/recipegenie/Profile.java
@@ -1,6 +1,10 @@
 package com.example.recipegenie;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +14,34 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Profile extends AppCompatActivity {
 
+    private ImageButton editProfileButton;
+    private ImageButton changePaswwordButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.profile), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        editProfileButton = findViewById(R.id.edit_profile);
+        changePaswwordButton = findViewById(R.id.change_password);
+
+        editProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the SecondActivity
+                Intent intent = new Intent(Profile.this, EditProfile.class);
+                startActivity(intent);
+            }
+        });
+
+        changePaswwordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the SecondActivity
+                Intent intent = new Intent(Profile.this, ChangePassword.class);
+                startActivity(intent);
+            }
         });
     }
 }
