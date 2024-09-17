17a593cd85b0fc83f0fb34009545de153a14c9c6
pasanmayura <karunat-im21020@stu.kln.ac.lk>
2024-09-17 12:42:33 +0530

    Added logic to open activity_set_new_password.xml

--- app/src/main/java/com/example/recipegenie/PasswordReset.java
+++ app/src/main/java/com/example/recipegenie/PasswordReset.java
@@ -1,6 +1,9 @@
 package com.example.recipegenie;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,5 +19,15 @@ public class PasswordReset extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_password_reset);
 
+        Button continueButton = findViewById(R.id.buttonReset);
+
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Start the SecondActivity
+                Intent intent = new Intent(PasswordReset.this, SetNewPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
