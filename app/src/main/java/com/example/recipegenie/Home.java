ac6ec6fe901c511d6cba9337eb89627bb733c168
pasanmayura <karunat-im21020@stu.kln.ac.lk>
2024-09-20 15:06:41 +0530

    added functions to view recipe cards

--- app/src/main/java/com/example/recipegenie/Home.java
+++ app/src/main/java/com/example/recipegenie/Home.java
@@ -1,24 +1,60 @@
 package com.example.recipegenie;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Home extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecipeAdapter adapter;
+    private List<Recipe> recipeList;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_home); // Make sure this is your home layout
+
+        recyclerView = findViewById(R.id.recipeRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        recipeList = new ArrayList<>();
+        adapter = new RecipeAdapter(recipeList);
+        recyclerView.setAdapter(adapter);
+
+        // Firebase database reference
+        databaseReference = FirebaseDatabase.getInstance().getReference("Recipe");
+
+        // Fetch data from Firebase
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                recipeList.clear();  // Clear list to avoid duplication
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Recipe recipe = snapshot.getValue(Recipe.class);
+                    recipeList.add(recipe);
+                }
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.e("Home", "Database error: " + databaseError.getMessage());
+            }
         });
     }
 }
