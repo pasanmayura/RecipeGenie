59ad0b5529411adf6b692aaa0946c30ae4f3cf3f
pasanmayura <karunat-im21020@stu.kln.ac.lk>
2024-09-23 16:27:01 +0530

    recipes are seen now

--- app/src/main/java/com/example/recipegenie/SavedRecipe.java
+++ app/src/main/java/com/example/recipegenie/SavedRecipe.java
@@ -7,22 +7,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+import java.util.List;
 
 public class SavedRecipe extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+
+    private RecyclerView recyclerView;
+    private RecipeAdapter adapter;
+    private List<Recipe> recipeList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_saved_recipe);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager2 = findViewById(R.id.view_pager);
+
+        VPAdapter vpAdapter = new VPAdapter(this);
+        //VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new fragment_SavedRecipes(),"Saved");
+        vpAdapter.addFragment(new fragment_MyRecipes(),"My Recipes");
+        viewPager2.setAdapter(vpAdapter);
+
+        new TabLayoutMediator(tabLayout, viewPager2,
+                (tab, position) -> {
+                    switch (position) {
+                        case 0:
+                            tab.setText("Saved");
+                            break;
+                        case 1:
+                            tab.setText("My Recipes");
+                            break;
+                    }
+                }
+        ).attach();
 
         // Set up Bottom Navigation
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
