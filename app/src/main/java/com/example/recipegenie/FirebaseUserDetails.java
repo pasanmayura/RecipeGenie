95784d5b10c2e5efd5111ef6b6ff918683465510
pasanmayura <karunat-im21020@stu.kln.ac.lk>
2024-09-30 13:39:02 +0530

    added the code to change user name

---
+++ app/src/main/java/com/example/recipegenie/FirebaseUserDetails.java
@@ -0,0 +1,14 @@
+package com.example.recipegenie;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
+public class FirebaseUserDetails {
+    // Reference to the current user's node in Firebase Realtime Database
+    public static DatabaseReference currentUserDetails() {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        String userId = auth.getCurrentUser().getUid();  // Get the current user's UID
+        return FirebaseDatabase.getInstance().getReference("Users").child(userId);
+    }
+}
